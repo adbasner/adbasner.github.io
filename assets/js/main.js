bdf17a69ae86a462a6927d366e6f80b707f813f9
Andrew Basner <andrewbasner@gmail.com>
2022-01-15 20:55:41 -0600

    make new navbar and connect all pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,33 +1,5 @@
 //************************************
-//* Navbar                           *
-//************************************
-
-
-//Targets toggle icon
-const toggle = document.querySelector('#toggle-icon');
-
-//Add onclick event to icon
-toggle.addEventListener('click', navbarToggle);
-
-//Function to show/hide nav items, called in navbarToggle
-function toggleResponsive(navItem) {
-  if (navItem.className === "nav-item") {
-    navItem.className += " responsive";
-  } else {
-    navItem.className = "nav-item";
-  }
-}
-
-//Selects .nav-items and calls toggleResponsive to show/hide them
-function navbarToggle() {
-  const NavItems = document.querySelectorAll(".nav-item");
-  //I converted this to an array for IE support, Array.from did not work. 
-  const NavItemsArray = Array.prototype.slice.call(NavItems);
-  NavItemsArray.forEach(toggleResponsive);
-}
- 
-//************************************
-//* Current Date                     *
+//* Current Date for Copyright       *
 //************************************ 
 
 //Selects date span
@@ -41,25 +13,12 @@ const year = currentDate.getFullYear();
 date.innerHTML = year;
 
 //************************************
-//* Text Carousel                    *
-//* Replaced with css animate        *
+//* Makes Hamburger work             *
 //************************************ 
 
-// let textIndex = 0;
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems);
+  console.log(instances);
+});
 
-// function carousel() {
-//   let texts = document.querySelectorAll(".i-am-a-text");
-//   for (let i = 0; i < texts.length; i++) {
-//   texts[i].style.display = "none"
-//   }
-
-//   textIndex++;
-
-//   if (textIndex > texts.length) {
-//     textIndex = 1;
-//   }
-//   texts[textIndex - 1].style.display = "block";
-//   setTimeout(carousel, 2000);
-// }
-
-// carousel();
